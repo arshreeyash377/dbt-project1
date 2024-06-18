49b67998b01a9ede36317461fa69bd2e601887d9
Aruna Gupta <arunashgupta@gmail.com>
2024-06-18 19:57:04 +0000

    added second comment on second dbtmodel that is referring to first dbt model

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,5 +1,6 @@
 
 -- Use the `ref` function to select from other models
+--it is actually using the referring the first model and is dependent on first model
 
 select *
 from {{ ref('my_first_dbt_model') }}
